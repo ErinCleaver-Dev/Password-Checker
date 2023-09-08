ee82759442b3ec4add55dd000795799a8ee11396
ErinCleaver-GRCC <77419665+ErinCleaver-GRCC@users.noreply.github.com>
2023-09-08 16:15:49 -0400

    password checker

--- main.py
+++ main.py
@@ -3,5 +3,4 @@
 #hackers use dictionary attacks
 import password_checker
 
-password_checker.request_api_data('CD415')
 password_checker.check_password_exist_api('password1234')
--- password_checker.py
+++ password_checker.py
@@ -11,7 +11,19 @@ def request_api_data(query_char):
         raise RuntimeError(f'Error fetching data from api: {res.status_code}')
     return res
 
+def get_leaked_passwords(hashes, hash_to_check):
+    hashes = (line.split(':') for line in hashes.text)
+    print(hashes)
+    for hash, count in hashes:
+        print(hash, count)
+    pass
 
-def check_password_exist_api(password):
-    print(password.encode('utf-8'))
-    #sha1password = hashlib.sha1(password.encode('utf-8'))
+def check_password_exist_api(password: str):
+    #must use utf8
+    sha1password = hashlib.sha1(password.encode('utf-8')).hexdigest().upper()
+
+    five_chars, tail = sha1password[:5], sha1password[5:]
+    
+    response = request_api_data(five_chars)
+    return get_leaked_passwords(response, tail)
+
