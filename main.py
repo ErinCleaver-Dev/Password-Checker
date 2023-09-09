5682e701a328e38a45c830de921d349dc18c66dc
ErinCleaver-GRCC <77419665+ErinCleaver-GRCC@users.noreply.github.com>
2023-09-09 09:01:04 -0400

    Created a password checker

--- main.py
+++ main.py
@@ -2,6 +2,22 @@
 import sys
 #hackers use dictionary attacks
 import password_checker
+import pathlib
 
-print(password_checker.check_passwords(sys.argv[1:]))
+filepath = pathlib.Path(__file__).parent / 'passwords.txt'
+
+
+passwords = []
+try:
+    with open(filepath, 'r') as my_password_file:
+        if(filepath.stat().st_size == 0):
+            print('No passwords found')
+        else:
+            for password in my_password_file.readlines():
+                passwords.append(password.strip())
+except:
+    print('No passwords found')
+
+
+print(password_checker.check_passwords(passwords))
 
